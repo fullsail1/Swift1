cdaf7bbe98ffeefa9b23603666db035168cfb9f5
Hicks_Anthony_SDI <osahon10@fullsail.edu>
2015-08-05 15:55:52 -0400

    first part
    on to part 2

--- lab/A_Hicks_lab1/A_Hicks_Lab1.playground/section-1.swift
+++ lab/A_Hicks_lab1/A_Hicks_Lab1.playground/section-1.swift
@@ -5,3 +5,24 @@ import UIKit
 var str = "Hello, playground"
 
 // Lab 1
+
+//Anthony Hicks
+//Lab 1
+//August 5, 2015
+
+//Explicitly
+
+var myIntExplicit: Int = 89;
+var myBoolExplicit: Bool = true
+let myConstantExplicit: String = "woo woo woo"
+var myDoubleExplicit: Double = 9.15324345
+let myConstantFloatExplicit: Float = 9.23344332323
+
+//Implicitly
+
+var myConstantInt = 89
+var myBoolImplicit = true
+var myArrayDouble = [89.009, 90.099]
+var myEmptyArrayBool = [Bool] ()
+var myArrayStrings = ["df", "fg", "gh"]
+
